b867f9503e0660b8ed2d1e885db0424bb6d77523
Srujana <p.srujana1356@gmail.com>
2024-07-11 11:10:26 +0530

    golf

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ class App extends Component {
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
+		    return <button onClick={this.buttonClickHandler} >start</button>
 		}
     }
     handleKeyDown(event) {
